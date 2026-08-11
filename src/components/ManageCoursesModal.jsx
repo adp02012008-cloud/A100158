7036bb53784e9edccf4f6da41c93823c2c7ec2a3
DHASHAPRAKASH <adp02012008@gmail.com>
2026-08-11 18:32:58 +0530

    fix: import missing useMemo hook in ManageCoursesModal

--- src/components/ManageCoursesModal.jsx
+++ src/components/ManageCoursesModal.jsx
@@ -1,5 +1,5 @@
 // src/components/ManageCoursesModal.jsx
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import { apiFetch } from "../utils/api";
 import EditCourseModal from "./EditCourseModal";
 import AddCourseModal from "./AddCourseModal";
